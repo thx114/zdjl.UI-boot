955f5253bd99092b79be702604897401bfd144d6
F_thx <1764362678@qq.com>
2023-02-20 14:24:34 +0800

    add

--- just.js
+++ just.js
@@ -529,21 +529,17 @@ class setvar extends Action {
           .js([[JS[0][0]+RUNLIST]])
      }   
     function textlist(obj){ let thisobj = object(false).t
-        let TEXTLISTLASTKEY = ''
         Object.entries(obj).forEach(([key, value],index)=>{
-            let key_ = key
-            if (key_[0][0].includes('#add')){key_=[[key_[0][0].replace('#add',TEXTLISTLASTKEY)]]}
             if(value[2]===0){thisobj.objectVars=[...thisobj.objectVars,...[
-                {name:`_${index}`,value:text(value[0]).h([[`!(${exp(key_)})`]]).BGcolor('#035d00').color('#07ea00')},
-                {name:`_${index}`,value:text(value[1]).h([[`${exp(key_)}`]]).BGcolor('#400300').color('#ff0005')}
+                {name:`_${index}`,value:text(value[0]).h([[`!(${exp(key)})`]]).BGcolor('#035d00').color('#07ea00')},
+                {name:`_${index}`,value:text(value[1]).h([[`${exp(key)}`]]).BGcolor('#400300').color('#ff0005')}
             ]]}
             else{thisobj.objectVars.push({name:`_${index}`,value:text(value[0])
-              .h([[`${exp(key_)}`]])
+              .h([[`${exp(key)}`]])
               .BGcolor(value[1]?'#035d00':'#400300')
               .color(value[1]?'#07ea00':'#ff0005')
             })}
-            TEXTLISTLASTKEY = key_[0][0]
-            console.log(exp(key_))
+            console.log(exp(key))
         })
         thisobj.apply({})
         return thisobj
