39eedb3722bec33b48d500d0fe5f1d2431416d0a
F_thx <1764362678@qq.com>
2023-02-20 13:42:15 +0800

    aa

--- just.js
+++ just.js
@@ -522,8 +522,8 @@ class setvar extends Action {
      }   
     function textlist(obj){ let thisobj = object(false).t
         Object.entries(obj).forEach(([key, value],index)=>{
-            let key_=key[0][0].replace(/#add#/,`!${Object.keys(obj)[index-1]}||`)
-            key=[[key_]]
+            // let key_=key[0][0].replace(/#add#/,`!${Object.keys(obj)[index-1]}||`)
+            // key=[[key_]]
             if(value[2]===0){thisobj.objectVars=[...thisobj.objectVars,...[
                 {name:`_${index}`,value:text(value[0]).h(Array.isArray(key)?[[`!(${exp(key)})`]]:[[`!(${key})`]]).BGcolor('#035d00').color('#07ea00')},
                 {name:`_${index}`,value:text(value[1]).h(Array.isArray(key)?[[`${exp(key)}`]]:[[key]]).BGcolor('#400300').color('#ff0005')}
