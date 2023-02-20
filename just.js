2e9aa2550332a0bda781835fbf88496f91597265
F_thx <1764362678@qq.com>
2023-02-20 12:24:21 +0800

    aaa

--- just.js
+++ just.js
@@ -201,9 +201,9 @@ function exp(keyname, value, apply) {
         let _id
         _id = id ?? window.id ?? global.id ?? zdjl.getVar('id')
         let out = value[0][0]
+            .replace(/from\s(.*)\sremove\s(.*);/,`$1.objectVars.splice($1.objectVars.findIndex(v=>v.name=='$2'),1));`)
             .replace(/#this/g, `all[${_id}]`)
             .replace(/this/g, `eval(all[${_id}].R)`)
-            .replace(/from\s(.*)\sremove\s(.*);/,"$1.objectVars.splice($1.objectVars.findIndex(v=>v.name=='$2'),1));")
         console.log(`Exp: ${out}  <${apply.name}>`)
         apply.e[keyname] = { valueExp: out, varType: 'expression' }
     }
