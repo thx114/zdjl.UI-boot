9816c57159339bf81951b9a02b5df310cc218390
F_thx <1764362678@qq.com>
2023-03-11 03:54:11 +0800

    Add files via upload

--- just2.js
+++ just2.js
@@ -123,7 +123,7 @@ class Obj {
         Obj.applys(this,objs)
 
      }
-}
+     }
 class Var{
     static value2Var(value){
         switch(typeof value){
@@ -170,40 +170,35 @@ class Var{
         if(str.length<1){str='zdjl.getVars()'}
         return str
      }
-
+    static exp(input){return {varType:'expression',valueExp:input}}
+    static varsC(obj){obj.__vars??={}}
     constructor(Var){
         this.output = Var
         this.S(true)
         this.SYNC(true)
-    }
+     }
     exp=(name,value)=>{
-        if (typeof value.output != 'undefined'){
-            value = value.output
-        }
-        let app = exp(value,true)
-        function self(input){
-            if (input instanceof Action||input instanceof Var){
-                input = input.output
-            }
-            return input
-        }
-        if (BaseTlist.addlist[name][3]==1){
-            if (app.mode==1){
-                delete this.output[name]
-                this.output.__vars ??={}
-                this.output.__vars[name] = app
-            }
-            else{
-                if(typeof this.output?.__vars?.[name] != 'undefined'){delete this.output?.__vars?.[name]}
-                this.output[name]=self(app.valueExp)
-            }
-        }
-        else{
-            this.output[name]=self(app.valueExp)
-        }
+        console.log(value)
+        if (typeof value.output != 'undefined'){value = value.output}
+        if (typeof this.output[name] != 'undefined'){delete this.output[name]}
+        if (typeof this.output?.__vars?.[name] != 'undefined'){delete this.output?.__vars?.[name]}
+        if (value instanceof Function){value = [value]} // Exp function
+        if (value instanceof Array && (value[0] instanceof Function)){ // Exp functions
+            value.forEach(i=>{
+            this.output.__vars??={}
+            this.output.__vars[name]??=Var.exp('')
+            this.output.__vars[name].valueExp+=`(${i.toString()})();`
+            }) ;console.log(`Exp:${this.output.__vars[name].valueExp}`)}
+        else if (value instanceof Array&&value[0] instanceof Array&&value[0][0] instanceof String){ // Exp strings
+            value[0].forEach(i=>{
+            this.output.__vars??={}
+            this.output.__vars[name]??=Var.exp('')
+            this.output.__vars[name].valueExp+=`(${i.toString()})();`
+            }) ;console.log(`Exp:${this.output.__vars[name].valueExp}`)}
+        
+        else{this.output[name]=value;;console.log(`Exp:${this.output[name]}`)} // else
         return this
-
-        }
+     }
     toJSON(){return `<Var> ${this.output.varType} :「${JSON.stringify(Object.values(this.output)[1])}」 ${JSON.stringify(this.output)}`}
     MORE(name,value){this.output[name]=value;return this}
     W=(any=100)=>{return this.exp('showInputWidthBasis',any)}
@@ -303,7 +298,7 @@ class setvar{
         setvar.scan(this,'vars')
         all[-1]=this
      }
-}
+     }
 class Action{
     static delay(obj,time,Dunit=0){
         if (typeof time == 'string'){ 
@@ -332,7 +327,7 @@ class Action{
     then(action){Action.then(this.output,action);return this}
     then_js(code){Action.then_js(this.output,code);return this}
     get run(){run(this.output);return this}
-}
+     }
 {
  function string(value='') {return new Var({varType:'string',value:value})}
  function number(value=0) {return new Var({varType:'number',number:value})}
@@ -352,105 +347,85 @@ class Action{
  function run(input){zdjl.runActionAsync(input)}
  function js(input){return new Action({type:'运行JS代码',jsCode:input})}
  
- function direct(input){return input}
- function set2(input,sets){
-    let SET = 0
-    let OUT
-    Object.entries(input).forEach(([key,val],index)=>{
-        if(sets[index] == String){ if(val.length>SET){SET = val.length;OUT = key} }
-    })
-    return input[OUT]
-  }
- function exp(input,need=false){
-    let out
-    let mode 
-    switch(typeof input){
-        case 'function': out= `(${input.toString()})()`;mode = 1;break
-        case 'object': 
-        if(input instanceof Array &&input[0] instanceof Array&&typeof input[0][0] == 'string'){out = input[0][0];mode = 1}
-        else{out = input;mode = 0};break
-        default: out= input;mode=0;break
-    }
-    if(need){return {varType:'expression',valueExp:out,mode:mode}}
-    else{return {varType:'expression',valueExp:out}}
- }
  function Switch(bool){
-    return button(bool).MORE('BOOLNAME',true).sync.c.style('none')
-    .text(()=>{
+    return button(bool).MORE('BOOLNAME',true).sync.c.style('none').text(()=>{
         switch(this['%BOOLNAME%']){
             case -2: return All.base64_to_img( BASE64_ZUI.off,G_Switch_size,'png' )
             case 2: return All.base64_to_img( BASE64_ZUI.on,G_Switch_size,'png' )
             case -1: return All.base64_to_img( BASE64_ZUI.go_off,G_Switch_size,'gif' )
             case 1: return All.base64_to_img( BASE64_ZUI.go_on,G_Switch_size,'gif' )
-            case false: this['%BOOLNAME%']=-2;return All.base64_to_img( BASE64_ZUI.on,G_Switch_size,'png' )
-            case true: this['%BOOLNAME%']=2;return All.base64_to_img( BASE64_ZUI.off,G_Switch_size,'png' )
-        }})
-    .js(()=>{
+            case false: this['%BOOLNAME%']=-2;return All.base64_to_img( BASE64_ZUI.off,G_Switch_size,'png' )
+            case true: this['%BOOLNAME%']=2;return All.base64_to_img( BASE64_ZUI.on,G_Switch_size,'png' )
+        }}).js(()=>{
         switch(this['%BOOLNAME%']){
             case -2:this['%BOOLNAME%']=1;js(()=>{this['%BOOLNAME%']=2}).delay(500).run;break
             case 2: this['%BOOLNAME%']=-1;js(()=>{this['%BOOLNAME%']=-2}).delay(500).run;break
         }})
   }
+ function infolist(){
+
+ }
+  
 }
 new Main()
-BaseTlist = {
-    funlist:{
-      string:               ['字符串',1,string,['value_string']],
-      number:               ['数字',1,number,['number']],
-      boolean:              ['布尔',1,bool,['value_boolean']],
-      object:               ['对象',0,object,['objectVars']],
-      func:                 ['函数',0,func,['jsCode']],
+// BaseTlist = {
+//     funlist:{
+//       string:               ['字符串',1,string,['value_string']],
+//       number:               ['数字',1,number,['number']],
+//       boolean:              ['布尔',1,bool,['value_boolean']],
+//       object:               ['对象',0,object,['objectVars']],
+//       func:                 ['函数',0,func,['jsCode']],
 
-      text:                 ['文本',1,text,['textContent']],
-      button:               ['按钮',1,button,['buttonText']],
-      image:                ['图片',0,image,['data']],
-      color:                ['颜色',1,color,['color']],
-      xy:                   ['位置',1,xy,['position']],
-      area:                 ['区域',1,area,['screen_area']],
-    },
-    addlist:{
-      value_string:         ['内容',direct,`string()`,1],
-      value_boolean:        ['布尔',direct,`bool()`,1],
-      value:                ['',direct,null,1],
-      number:               ['数字',direct,`number()`,1],
-      objectVars:           ['对象',direct,`text('未实现')`,0],
-      jsCode:               ['',direct,`func()`,0],
+//       text:                 ['文本',1,text,['textContent']],
+//       button:               ['按钮',1,button,['buttonText']],
+//       image:                ['图片',0,image,['data']],
+//       color:                ['颜色',1,color,['color']],
+//       xy:                   ['位置',1,xy,['position']],
+//       area:                 ['区域',1,area,['screen_area']],
+//     },
+//     addlist:{
+//       value_string:         ['内容',direct,`string()`,1],
+//       value_boolean:        ['布尔',direct,`bool()`,1],
+//       value:                ['',direct,null,1],
+//       number:               ['数字',direct,`number()`,1],
+//       objectVars:           ['对象',direct,`text('未实现')`,0],
+//       jsCode:               ['',direct,`func()`,0],
 
-      textContent:          ['内容',direct,`string()`,1],
-      buttonText:           ['按钮文字',direct,`string()`,1],
-      data:                 ['图片内容',direct,`image()`,0],
-      color:                ['颜色',set2,[`object({0:color(),1:string()})`,[String,String]],1],
-      position:             ['位置',direct,`xy()`,1],
-      screen_area:          ['区域',direct,`area()`,1],
+//       textContent:          ['内容',direct,`string()`,1],
+//       buttonText:           ['按钮文字',direct,`string()`,1],
+//       data:                 ['图片内容',direct,`image()`,0],
+//       color:                ['颜色',set2,[`object({0:color(),1:string()})`,[String,String]],1],
+//       position:             ['位置',direct,`xy()`,1],
+//       screen_area:          ['区域',direct,`area()`,1],
 
-      showInputWidthBasis:  ['显示宽度',direct,`string('auto').list(['100%','95%','90%','85%','80%','75%','70%','65%','60%','55','50%','45%','40%','35%','30%','25%','20%','15%','10%','5%','0%'])`,1],
-      showInputWidthGrow:   ['撑满空间',direct,`number(0)`,1],
-      showInputHiddenView:  ['隐藏显示',direct,`bool()`,1],
-      textSize:             ['文字大小',direct,`number(13)`,1],
-      textColor:            ['文字颜色',set2,[`object().apply({v1:color().wa.t.h([["typeof this.v2 !='undefined' && this.v2.length>1"]]),v2:string().wa.t.h([['typeof this.v1=="number"']])})`,[String,String]],1],
-      action:               ['按钮动作',set2,["object().apply({v1:action().wa.h([[`typeof this.v2 !='undefined' && this.v2.length>1`]]),v2:string().wa.h([[`typeof this.v1 !='undefined' && this.v1.type>0`]])})",[Action,String]],0],
-      stringItems:          ['选择列表',direct,`list()`,1],
-      buttonStyle:          ['按钮外观',direct,`string().list(['button','link','none'])`,0],
-      buttonText:           ['按钮文字',direct,`string()`,1],
-      textLineBefore:       ['上文本',direct,`string()`,1],
-      textLineAfter:        ['下文本',direct,`string()`,1],
-      showInputLabel:       ['标题',direct,`string()`,1],
-      textAppendRight:      ['右文本',direct,`string()`,1],
-      backgroundColor:      ['背景颜色',direct,["object().apply({v1:color().wa.t.h([[`typeof this.v2 !='undefined' && this.v2.length>1`]]),v2:string().wa.t.h([[`typeof this.v1=='number'`]])})",[String,String]],1],
-      backgroundImg:        ['背景图片',direct,`image()`,1],
-      showInputContentAlign:['位置',direct,`xy()`,1],
-      onlyForShow:          ['仅展示',direct,`bool()`,0],
-      showInput:            ['显示变量',direct,`bool()`,0],
-      mustInput:            ['必须输入',direct,`bool()`,0],
-      syncValueOnChange:    ['SYNC',direct,`bool()`,0],
-      showInputHiddenLabel: ['隐藏标题',direct,`bool()`,0],
-      closeDialogOnAction:  ['按钮关闭界面',direct,`bool()`,0],
+//       showInputWidthBasis:  ['显示宽度',direct,`string('auto').list(['100%','95%','90%','85%','80%','75%','70%','65%','60%','55','50%','45%','40%','35%','30%','25%','20%','15%','10%','5%','0%'])`,1],
+//       showInputWidthGrow:   ['撑满空间',direct,`number(0)`,1],
+//       showInputHiddenView:  ['隐藏显示',direct,`bool()`,1],
+//       textSize:             ['文字大小',direct,`number(13)`,1],
+//       textColor:            ['文字颜色',set2,[`object().apply({v1:color().wa.t.h([["typeof this.v2 !='undefined' && this.v2.length>1"]]),v2:string().wa.t.h([['typeof this.v1=="number"']])})`,[String,String]],1],
+//       action:               ['按钮动作',set2,["object().apply({v1:action().wa.h([[`typeof this.v2 !='undefined' && this.v2.length>1`]]),v2:string().wa.h([[`typeof this.v1 !='undefined' && this.v1.type>0`]])})",[Action,String]],0],
+//       stringItems:          ['选择列表',direct,`list()`,1],
+//       buttonStyle:          ['按钮外观',direct,`string().list(['button','link','none'])`,0],
+//       buttonText:           ['按钮文字',direct,`string()`,1],
+//       textLineBefore:       ['上文本',direct,`string()`,1],
+//       textLineAfter:        ['下文本',direct,`string()`,1],
+//       showInputLabel:       ['标题',direct,`string()`,1],
+//       textAppendRight:      ['右文本',direct,`string()`,1],
+//       backgroundColor:      ['背景颜色',direct,["object().apply({v1:color().wa.t.h([[`typeof this.v2 !='undefined' && this.v2.length>1`]]),v2:string().wa.t.h([[`typeof this.v1=='number'`]])})",[String,String]],1],
+//       backgroundImg:        ['背景图片',direct,`image()`,1],
+//       showInputContentAlign:['位置',direct,`xy()`,1],
+//       onlyForShow:          ['仅展示',direct,`bool()`,0],
+//       showInput:            ['显示变量',direct,`bool()`,0],
+//       mustInput:            ['必须输入',direct,`bool()`,0],
+//       syncValueOnChange:    ['SYNC',direct,`bool()`,0],
+//       showInputHiddenLabel: ['隐藏标题',direct,`bool()`,0],
+//       closeDialogOnAction:  ['按钮关闭界面',direct,`bool()`,0],
 
 
-    }
+//     }
 
-}
-let list =[Action,Var,Obj,setvar,All, string,number,bool,object,func, button,image,color,xy,area,text, setvars,run,js,direct,set2,exp]
+// }
+let list =[Action,Var,Obj,setvar,All, string,number,bool,object,func, button,image,color,xy,area,text, setvars,run,js,Switch]
 list.forEach(i=>{window[i.name]=i})
 
 
